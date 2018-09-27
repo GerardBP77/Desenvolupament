734acd59f8ad1d3ac697e262e377580580ee5081
GerardBP77 <GerardBP77@users.noreply.github.com>
2018-09-27 23:34:23 +0200

    Adding string class

---
+++ String class/Project1/Project1/String.h
@@ -0,0 +1,36 @@
+#ifndef _STRING
+#define _STRING
+
+#include <iostream>
+
+class String {
+public:
+
+	char* text;
+
+	String() {
+		text = (char*)"";
+	};
+
+	String(const char* txt) : text((char*)txt) {};
+
+	String(const String& txt) : text(txt.text) {};
+
+	String operator=(const char* txt) {
+		return text = (char*)txt;
+	}
+
+	String operator=(const String& txt) {
+		return text = txt.text;
+	}
+
+	bool operator==(const String& txt) const {
+		return !strcmp(text, txt.text);
+	}
+
+	bool operator==(const char* txt) const {
+		return !strcmp(text, txt);
+	}
+
+};
+#endif // !_STRING
